32ed92ba7c53755d30f1f26abafe6c39025779a0
rpotopacademy-lang <rpotopacademy@gmail.com>
2026-06-10 13:07:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+src/main.py
+from helpers import load_tasks, print_tasks
+
+
+def main():
+    tasks = load_tasks("data/tasks.txt")
+    print("=== Student Task Journal ===")
+    print_tasks(tasks)
+
+
+if __name__ == "__main__":
+    main()
